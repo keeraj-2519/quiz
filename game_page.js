629ae01b9b362190422bfd07f306473534d5ab1e
keerthi <146065976+keeraj-2519@users.noreply.github.com>
2023-10-12 17:48:23 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,9 @@
+function multiNum(){
+    number1=document.getElementById("number1").value;
+    number2=document.getElementById("number2").value;
+    result=number1*number2;
+    
+    document.getElementById("result").textContent="Result" + result;
+    
+    
+}
